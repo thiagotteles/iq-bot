9f381bd5727f9fb151b2a11adbcf4ccaaeb77dfc
Thiago Teles (AWS) <thiagojames_@hotmail.com>
2020-11-25 10:42:37 -0300

    Print do evento

--- teste.py
+++ teste.py
@@ -2,28 +2,53 @@ from bs4 import BeautifulSoup
 import requests
 import time, json
 from datetime import datetime, timedelta
-import pyodbc 
-conn = pyodbc.connect('Driver={SQL Server};'
-					  'Server=localhost\SQLEXPRESS;'
-                      'Database=iqBot2;'
-                      'Trusted_Connection=yes;')
+# import pyodbc 
 
-par = 'GBPUSD'
-cor = 'd'
-cursor = conn.cursor()
+noticias = []
 
-cursor.execute('INSERT INTO [dbo].[velaM5] ([par] ,[abertura] ,[cor])   VALUES (\''+ par + '\',getdate(),\'' + cor +'\') ''')
-# print('''
-#                 INSERT INTO [dbo].[velaM5] ([par] ,[abertura] ,[cor]) 
-#                 VALUES
-#                 ('{par}',getdate(),'{cor}'),
-#                 ('{par}',getdate(),'{cor}')
-#                 ''')
+headers = requests.utils.default_headers()
+headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0'})
 
-cursor.execute('SELECT * FROM iqBot2.dbo.velaM5')
+data = requests.get('http://br.investing.com/economic-calendar/', headers=headers)
+
+if data.status_code == requests.codes.ok:
+    info = BeautifulSoup(data.text, 'html.parser')
+    
+    blocos = ((info.find('table', {'id': 'economicCalendarData'})).find('tbody')).findAll('tr', {'class': 'js-event-item'})
+    
+    for blocos2 in blocos:
+        impacto = str((blocos2.find('td', {'class': 'sentiment'})).get('data-img_key')).replace('bull', '')
+        horario = str(blocos2.get('data-event-datetime')).replace('/', '-')
+        moeda = (blocos2.find('td', {'class': 'left flagCur noWrap'})).text.strip()
+        evento = (blocos2.find('td', {'class': 'left event'})).text.strip()
+        
+        # noticias.append({ 'evento': evento, 'par': moeda, 'horario': horario, 'impacto': impacto})
+        print({ 'evento': evento, 'par': moeda, 'horario': horario, 'impacto': impacto})
+
+
+######### SQL ###########
+
+# conn = pyodbc.connect('Driver={SQL Server};'
+# 					  'Server=localhost\SQLEXPRESS;'
+#                       'Database=iqBot2;'
+#                       'Trusted_Connection=yes;')
+
+# par = 'GBPUSD'
+# cor = 'd'
+# cursor = conn.cursor()
+
+# cursor.execute('INSERT INTO [dbo].[velaM5] ([par] ,[abertura] ,[cor])   VALUES (\''+ par + '\',getdate(),\'' + cor +'\') ''')
+# # print('''
+# #                 INSERT INTO [dbo].[velaM5] ([par] ,[abertura] ,[cor]) 
+# #                 VALUES
+# #                 ('{par}',getdate(),'{cor}'),
+# #                 ('{par}',getdate(),'{cor}')
+# #                 ''')
+
+# cursor.execute('SELECT * FROM iqBot2.dbo.velaM5')
  
-for row in cursor:
-    print(row)
+# for row in cursor:
+#     print(row)
 
 
 ######### base de emails ###########
